745ca1306e59764a31fa52c0731aa326b5073c61
rayfiyo <108730891+rayfiyo@users.noreply.github.com>
2022-09-13 15:13:19 +0900

    bat file add - 2022/09/13

---
+++ MySetUpTool/wingetリスト.bat
@@ -0,0 +1,68 @@
+chcp 65001
+echo Microsoftストアにて "app installer" の更新を確認済みですか？
+
+echo PowerShell 7
+winget install -e --id Microsoft.PowerShell
+echo .
+
+
+
+echo DevToys
+winget install -e --id 9PGCV4V3BK4W
+echo .
+
+echo draw.io
+winget install -e --id JGraph.Draw
+echo.
+
+echo FireFox
+winget install -e --id Mozilla.Firefox
+echo .
+
+echo ★Google日本語入力
+winget install -e --id Google.JapaneseIME
+echo .
+
+echo PowerToys
+winget install -e --id Microsoft.PowerToys
+echo .
+
+echo VMware WorkstationPlayer
+winget install -e --id VMware.WorkstationPlayer
+echo .
+
+
+
+echo Git
+winget install -e --id Git.Git
+echo .
+
+echo Python3
+winget install -e --id Python.Python.3
+echo.
+
+
+
+echo Audacity
+winget install -e --id Audacity.Audacity
+echo .
+
+echo ★Discord
+winget install -e --id Discord.Discord
+echo .
+
+echo EarTrumpet
+winget install -e --id File-New-Project.EarTrumpet
+echo .
+
+echo LINE
+winget install -e --id LINE.LINE
+echo .
+
+echo Thunderbird
+winget install -e --id Mozilla.Thunderbird
+echo.
+
+echo VLC
+winget install -e --id VideoLAN.VLC
+echo.
