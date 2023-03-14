567e42450be3291ccb392c5c8e65af1faa23105a
rayfiyo <108730891+rayfiyo@users.noreply.github.com>
2023-03-15 01:00:22 +0900

    fix for kali

--- Ubuntu-setup/FirstRun.sh
+++ Ubuntu-setup/FirstRun.sh
@@ -6,33 +6,45 @@ echo "- neovim のインストール"
 echo "- fish(shell) と fisher(プラグインマネージャー) のインストール"
 echo "- fish のプラグインのインストール"
 
-
 echo "y オプション で apt-get update と upgrade"
 sudo apt-get update
 sudo apt-get upgrade -y
 
-
-
-echo "neovim のインストール"
-sudo add-apt-repository ppa:neovim-ppa/stable -y
+echo "add-apt-repository のインストール"
+sudo apt-get install software-properties-common -y
 sudo apt-get update
-sudo apt install neovim -y
+# 右のようなエラーが出る場合 AttributeError: 'NoneType' object has no attribute 'people'
+#	sudo apt-get install python3-launchpadlib
 
+echo "ppaを使わずに最新のneovim のインストール"
+curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
+sudo apt install ./nvim-linux64.deb -y
+rm nvim-linux64.deb
+# Ubuntu向け (ppa使う)
+#	sudo add-apt-repository ppa:neovim-ppa/stable -y
+#	sudo apt-get update
+#	sudo apt install neovim -y
 
-
-echo "fish のインストール"
+echo "Ubuntu向け fish のインストール(ppa使う)"
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish
+# Debian 11 向け (https://software.opensuse.org/download.html?project=shells%3Afish&package=fish)
+#	echo 'deb http://download.opensuse.org/repositories/shells:/fish/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish.list
+#	curl -fsSL https://download.opensuse.org/repositories/shells:fish/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish.gpg > /dev/null
+#	sudo apt update
+#	sudo apt install fish
 
 echo "fisher のインストール"
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
-
-
 echo "fish のプラグインのインストール"
 echo "fishプラグイン: edc/bass"
 fisher install edc/bass
+echo "fishプラグイン: 0rax/fish-bd"
+fisher install 0rax/fish-bd
+echo "fishプラグイン: jethrokuan/z"
+fisher install jethrokuan/z
 
 echo "fishプラグイン: jethrokuan/fzf"
 fisher install jethrokuan/fzf
@@ -40,7 +52,10 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 fisher install jethrokuan/z
 
-
+echo "デフォルトのshellをfishに"
+cat /etc/shells | grep fish
+echo "↑この結果を踏まえて次のコマンドのPathを変更して実行"
+echo "chsh -s /usr/bin/fish"
 
 echo "最終的な ppa の確認"
 sudo apt-add-repository --list
